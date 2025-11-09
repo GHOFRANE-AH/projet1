78509518f81941d32aa7430d28deec29e1aeeecd
GHOFRANE-AH <ghofraneah25@gmail.com>
2025-11-09 21:16:14 +0100

    Corriger CORS pour Netlify et localhost

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,18 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// ✅ CORS configuré pour autoriser ton frontend Vercel
+const allowedOrigins = [
+  'https://ghofrane-lense.netlify.app',
+];
+
 app.use(cors({
-  origin: 'https://ghofrane-lense.netlify.app', 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 // ✅ Middleware pour lire les données JSON
